f3c5d65d6d343802181b71a3b72baff6d6c441aa
Andrew Naumann <andrewnaumann@Andrews-MacBook-Pro.local>
2016-03-26 15:26:01 +0100

    Initial commit version to 0.1.0

---
+++ app.js
@@ -0,0 +1,126 @@
+
+var Vue = require('vue/dist/vue.js');
+var shell = require('electron').shell;
+var ipc = require('electron').ipcRenderer;
+var DRIBBBLE_ACCESS_TOKEN = "7db78f5cb574b2309af3ea772628b42269c6bfb6767b88ba13f5294d5b9cbb8d";
+var initialAPIURL = 'https://api.dribbble.com/v1/shots';
+var nextPageURL = "";
+var dribbbleJSON = {
+  shotlist:[]
+};
+var nextPageURL;
+var closeEl = document.querySelector('#close');
+var scrollTimer = null;
+var refreshTimer = null;
+var refreshTimeout = 300000; // 5 minutes
+var dataIsStale = false;
+
+
+var vm = new Vue({
+  el: "#shot-list",
+  data: {shotlist: []},
+  computed: {
+    hidpiMode: function () {
+      return true;
+    }
+  },
+  methods: {
+    openURL: function(url) {
+      shell.openExternal(url);
+    },
+    resetState: function() {
+      this.shotlist = [];
+      requestDribbbleJSON(initialAPIURL);
+    }
+  },
+  ready: requestDribbbleJSON(initialAPIURL)
+});
+
+var btn = new Vue({
+  el: "#next-button",
+  methods: {
+    loadNextPage: function(event) {
+      requestDribbbleJSON(nextPageURL);
+    }
+  }
+});
+
+var loadScreen = new Vue({
+  el: "#load-screen",
+  data: {
+    display: true
+  }
+});
+
+closeEl.addEventListener('click', function () {
+    ipc.sendSync('close-main-window');
+});
+
+ipc.on("app-shown", function() {
+  refreshAppData();
+});
+
+function refreshAppData() {
+  if (refreshTimer) {
+    clearTimeout(refreshTimer);
+  }
+  refreshTimer = setTimeout(setDataIsStale, refreshTimeout);
+
+  if (dataIsStale) {
+    resetAppState();
+  }
+}
+
+function setDataIsStale() {
+  dataIsStale = true;
+}
+
+function resetAppState() {
+  loadScreen.display = true;
+  refreshTimer = null;
+  vm.resetState();
+  document.body.scrollTop = 0;
+  dataIsStale = false;
+}
+
+document.addEventListener('scroll', function () {
+    if (scrollTimer) {
+        clearTimeout(scrollTimer);   // clear any previous pending timer
+    }
+    scrollTimer = setTimeout(handleScroll, 500);   // set new timer
+});
+
+function reqListener () {
+  var nextPageURLString = this.getResponseHeader('Link');
+  nextPageURL = getNextURL(nextPageURLString);
+  dribbbleJSON.shotlist = JSON.parse(this.responseText);
+  loadScreen.display = false;
+  for (var i = 0; i < dribbbleJSON.shotlist.length; i ++) {
+    vm.shotlist.push(dribbbleJSON.shotlist[i]);
+  }
+}
+
+function getNextURL(headerString) {
+  var semicolonPos = headerString.search(';') - 1;
+  var url =  headerString.substring(1, semicolonPos);
+  return url;
+}
+
+function requestDribbbleJSON(url){
+  var oReq = new XMLHttpRequest({});
+  oReq.addEventListener("load", reqListener);
+  oReq.open("GET", url);
+  oReq.setRequestHeader('Authorization', 'Bearer ' + DRIBBBLE_ACCESS_TOKEN);
+  oReq.send();
+}
+
+function handleScroll() {
+    scrollTimer = null;
+    var bodyHeight = document.body.scrollHeight;
+    var windowHeight = window.innerHeight;
+    var scrollPos = document.body.scrollTop;
+
+    if ((bodyHeight - windowHeight - 200) < scrollPos) {
+      btn.loadNextPage();
+    }
+}
--- main.js
+++ main.js
@@ -1,51 +1,55 @@
 'use strict';
-
 const electron = require('electron');
-// Module to control application life.
+const ipc = require('electron').ipcMain;
+const menubar = require('menubar');
 const app = electron.app;
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow;
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
+const mb = menubar({
+  icon: __dirname + '/HotshotIcon2.png',
+  width: 428,
+  height: 700,
+  minWidth: 300,
+  maxWidth: 828,
+  "show-dock-icon": true,
+  'preload-window': true
+});
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+var windowIsShown = false;
 
-  // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
+function toggleShowWindow() {
+  if (windowIsShown) {
+    mb.hideWindow();
+    windowIsShown = false;
+  } else {
+    mb.showWindow();
+    windowIsShown = true;
+  }
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-app.on('ready', createWindow);
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
+  toggleShowWindow();
+  sendWindowShownMessage();
 });
+
+mb.on('ready', function() {
+  // mb.window.openDevTools();
+});
+
+mb.on('show', function() {
+  windowIsShown = true;
+  sendWindowShownMessage();
+});
+
+mb.on('hide', function() {
+  windowIsShown = false;
+});
+
+ipc.on('close-main-window', function () {
+    app.quit();
+});
+
+function sendWindowShownMessage() {
+  mb.window.webContents.send("app-shown");
+}
